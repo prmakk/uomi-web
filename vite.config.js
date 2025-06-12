3ffb530f2678c6a1aad3c03b03a69da02010c21f
jacko06v <jacomosconi@gmail.com>
2025-06-12 18:03:52 +0200

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import mdx from '@mdx-js/rollup';
+import mdx from '@mdx-js/rollup'
+import { resolve } from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react(), mdx()],
+  base: '/',
   build: {
-    outDir: 'dist'
-  },
-  base: '/'
+    outDir: 'docs',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        ecosystem: resolve(__dirname, 'ecosystem.html'),
+      }
+    }
+  }
 })
